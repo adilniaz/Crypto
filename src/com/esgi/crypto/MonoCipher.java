b5af69f1ae6c1aaed5d73981f5b1d6467f1b28c1
mfiari <fiarimike@yahoo.fr>
2015-06-22 17:46:01 +0200

    encode

--- src/com/esgi/crypto/MonoCipher.java
+++ src/com/esgi/crypto/MonoCipher.java
@@ -1,6 +1,8 @@
 package com.esgi.crypto;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -24,8 +26,22 @@ public class MonoCipher implements ICipher {
 	
 	@Override
 	public void encode(File message, File key, File encoded) {
-		// TODO Auto-generated method stub
-		
+		BufferedReader br = null;
+		try {
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(message));
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 
 	@Override
