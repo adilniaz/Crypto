f7e81f7463b875bcb498000d78552d0f99f8902a
adilniaz <niaz.adil1@gmail.com>
2015-06-23 15:26:38 +0200

    created CeasarCipher

---
+++ src/com/esgi/crypto/CaesarCipher.java
@@ -0,0 +1,26 @@
+package com.esgi.crypto;
+
+import java.io.File;
+
+public class CaesarCipher {
+	
+	public char gap;
+	public String array = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	private void encode(File message, char gap, File encoded) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void decode(File encoded, char gap, File message) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void decipher(File encoded, File message) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+}
