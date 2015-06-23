57624ec14fd080e005f8985654758eac24396e1d
adilniaz <niaz.adil1@gmail.com>
2015-06-23 10:26:15 +0200

    attack file created

---
+++ src/com/esgi/crypto/MonoEncodedAttack.java
@@ -0,0 +1,10 @@
+package com.esgi.crypto;
+
+import java.io.File;
+
+public class MonoEncodedAttack {
+	private void findKey(File encoded, File foundKey) {
+		// TODO Auto-generated method stub
+		
+	}
+}
