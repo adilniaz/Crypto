68a98d2da25168c45d17f99e57f41246aac862f8
mfiari <fiarimike@yahoo.fr>
2015-06-26 16:58:16 +0200

    modif

--- src/org/esgi/crypto/AttackTransposition.java
+++ src/org/esgi/crypto/AttackTransposition.java
@@ -143,21 +143,6 @@ public class AttackTransposition implements IExecute {
 		}
 		return factors;
 	}
-
-	public void permutation(String str) {
-	    permutation("", str);
-	}
-
-	private void permutation(String prefix, String str) {
-	    int n = str.length();
-	    if (n == 0) {
-	    	list.add(prefix);
-	    	
-	    } else {
-	        for (int i = 0; i < n; i++)
-	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-	    }
-	}
 	
 	public HashMap<Character, Integer> calculateCharacterFrequency(File file) {
 		String encodedMessage = fH.readFile(file);
